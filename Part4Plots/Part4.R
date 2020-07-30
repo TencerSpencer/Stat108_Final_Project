bbb42ad340700c65111191b887085cbaf541ed62
TencerSpencer <57374486+TencerSpencer@users.noreply.github.com>
2020-07-29 23:24:31 -0700

    adding notes for part 4 approach

--- Part4Plots/Part4.R
+++ Part4Plots/Part4.R
@@ -13,6 +13,10 @@ transformed_dat[,4] <- (transformed_dat[,3])^(1/3)
 transformed_line <- lm(transformed_dat$MORTALITY ~ ., dat = transformed_dat)
 
 
+# Maybe we transform the data slowly and compare R^2? R^2/R^2adj is a good indicator
+# If the # of vars does not change it I recall correctly. This will greatly help our analysis.
+
+
 # PART 5 WILL BE TO CONDUCT TESTS/STEP REGRESSION TO CHECK DATA
 
 # ESSENTIALY, COPY/TRIAL MOST OF THE CODE FROM PART 2. TEST MATRIX CORRELATION FIRST...
